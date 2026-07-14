09a8879ef4e2ac6c77933299ca0fa805104833ee
Arshpreet094 <singharshpreet9460@gmail.com>
2026-07-14 11:25:27 +0530

    Skills Education Project

--- src/main/java/com/arshpreet/demo/Portfolio/MyPortfolio.java
+++ src/main/java/com/arshpreet/demo/Portfolio/MyPortfolio.java
@@ -17,5 +17,39 @@ public class MyPortfolio {
                 </ul>
                 """;
     }
+    @GetMapping("/skills")
+    public String skills(){
+        return """
+                <h1>Skills>/h1>
+                <p>Languages</p>
+                <ul>
+                <li>C++</li>
+                <li>Python</li><li>Java</li>
+                </ul>
+                
+                <p>Tools and Technologies</p>
+                <ul>
+                <li>Git</li>
+                <li>PowerBI</li><li>PL/SQL</li>
+                </ul>
+                """;
+    }
+    @GetMapping("/education")
+    public String education(){
+        return """
+                <h1>Education</h1>
+                <p>Lovely Professional University</p>
+                """;
+    }
+    @GetMapping("/projects")
+    public String projects(){
+        return """
+                <h1>Projects</h1>
+                <ul>
+                <li>1. ML Project</li>
+                <li>2. PowerBI DashBoard</li>
+                </ul>
+                """;
+    }
 
 }

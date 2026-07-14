5c93319a82e694d879d41609d0f8e7c04f1e14a9
Arshpreet094 <singharshpreet9460@gmail.com>
2026-07-14 12:58:05 +0530

    Creating Student with PostMan

--- src/main/java/com/arshpreet/demo/StudentServer/StudentServer.java
+++ src/main/java/com/arshpreet/demo/StudentServer/StudentServer.java
@@ -1,6 +1,7 @@
 package com.arshpreet.demo.StudentServer;
 
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
@@ -8,13 +9,18 @@ public class StudentServer {
 
     //1. Store the student info (Post)
     @PostMapping("/create")
-    public String storeStudent(){
-        return """
-                id : 1
-                name : Arshpreet
-                Department : CSE
-                age : 21
-                """;
+    public String storeStudent(@RequestBody Student student){
+
+        int id = student.getId();
+        String name = student.getName();
+        int age = student.getAge();
+        String department = student.getDepartment();
+
+
+        return " id : " + id +
+                ", name : " + name +
+                ",Department : " + department +
+                ",age : " + age;
     }
 
     //2. Read the student info (Get)

c81af02e1d5a43f953590a648ee02c3e3f3b4afc
Arshpreet094 <singharshpreet9460@gmail.com>
2026-07-15 11:27:39 +0530

    Data JPA

--- src/main/java/com/arshpreet/demo/StudentServer/StudentRepository.java
+++ src/main/java/com/arshpreet/demo/StudentServer/StudentRepository.java
@@ -1,12 +1,13 @@
 package com.arshpreet.demo.StudentServer;
 
+import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
-public class StudentRepository {
+public interface StudentRepository extends JpaRepository<Student, Integer> {
 
-    public Student save(Student student){
-        System.out.println("Student data saved!");
-        return student;
-    }
+//    public Student save(Student student){
+//        System.out.println("Student data saved!");
+//        return student;
+//    }
 }
